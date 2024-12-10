c2df991fb6a986d3a369f1d505d8c9fb93e148f5
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-10 15:02:46 +0530

    font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	important: true,
 	theme: {
 		extend: {
 			fontFamily: {
-        		pixel: ['pixel', 'sans-serif'],
-        		gothic: ['gothic', 'sans-serif'],
-        		iso: ['iso', 'sans-serif'],
+        		pixel: ['Pixelated Elegance', 'sans-serif'],
+        		gothic: ['Gothic A1', 'sans-serif'],
+        		iso: ['ISOCPEUR', 'sans-serif'],
       		},
 			colors: {
 				'bg-default': '#1A1A1A',
